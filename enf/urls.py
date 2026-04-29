d83dda45964f71f556fdb05c87de40120a21d050
studalbert <albertik_1098@mail.ru>
2026-04-30 04:20:20 +0500

    Add users URL pattern to include user-related views
    - Added a new URL pattern for the users app in urls.py, allowing access to user-related views under the 'users/' namespace.

--- enf/urls.py
+++ enf/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
     path('', include(('main.urls', 'main'), namespace='main')),
 ]
 
